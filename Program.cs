13b8092d60c65a82564a52984f1970b5fb9d4588
aLearningLad <mocmanca@gmail.com>
2024-10-19 13:21:00 +0200

    initial files

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the SHARP-DROME!");
+
+Console.WriteLine("Please provide a word to check: ");
+string targetWord = Console.ReadLine();
+
+Console.WriteLine($"You provided the word {targetWord}...");
